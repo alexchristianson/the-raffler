59c255bba02391d9a4b52390c9fd209002c0933c
ethan fregien <ethanfregien@gmail.com>
2022-02-03 13:42:51 -0600

    making raffle products, started card to display them and hunting for more images

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,20 @@
+function Card(props) {
+    return (
+      <div className="card col-md-5 col-sm-12 ms-4 mb-5 border border-2 border-dark">
+          <div className="img-container">
+              <img alt={props.name} src={props.image} />
+          </div>
+          <div className="card-body">
+          
+              <h5 className='card-title'>
+                  {props.name}
+              </h5>
+              <h6 className='card-text'>
+                  {props.description}
+              </h6>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Card;
