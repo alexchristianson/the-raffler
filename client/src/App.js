16ec4f2a7170a418eadfcf215d491a1998e82d74
ethan fregien <ethanfregien@gmail.com>
2022-02-03 15:21:20 -0600

    images for raffle

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Home from "./pages/Home";
 import Nav from "./components/Nav";
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
--- client/src/components/Raffles/index.js
+++ client/src/components/Raffles/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import img1 from '';
-import img2 from '';
-import img3 from '';
-import img4 from '';
+import img1 from '../../assets/img/cuc.jpg';
+import img2 from '../../assets/img/shrekbeard.jpg';
+import img3 from '../../assets/img/loan.jpg';
+import img4 from '../../assets/img/angrycat.jpg';
 import img5 from '';
 
 
 
-const Products = [
+const products = [
     {
-        
+        name: "Cucumber",
+        description: "Sweet Savory Cucumber",
+        image: img1
     },    
     {
-        
+        name: "Shrek Beard",
+        description: "Painting of Shrek Beard, complete with matching fedora",
+        image: img2,
     },
     
     {
-        
+        name: "Payday Loan",
+        description: "Payday loan varying from $5-500",
+        image: img3
     },     
     {
-        
+        name: "Angry cat",
+        description: "Angry fluffy cat",
+        image: img4
     },  
     {
-       
+        name: "Quarter",
+        Description: "Shiny new quarter",
+        image: img5
     },    
     
 ]
