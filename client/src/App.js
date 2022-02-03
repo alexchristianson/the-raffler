87613dd528de0886ad76f7f7de3863099b741193
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2022-02-03 12:08:05 -0600

    added auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Nav from "./components/Nav";
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-    </div>
+    <Router>
+      <div>
+          <Nav />
+          <Routes>
+            <Route exact path='/login' element={<Login/>}/>
+            <Route exact path='/signup' element={<SignUp/>}/>
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
