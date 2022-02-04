b295222a77537f9987f64046dc243f1ed1425942
ethan fregien <ethanfregien@gmail.com>
2022-02-03 20:34:43 -0600

    mui

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Nav from "./components/Nav";
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Footer from './components/Footer';
+import Raffles from './components/Raffles';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
             <Route exact path='/login' element={<Login/>}/>
             <Route exact path='/signup' element={<SignUp/>}/>
           </Routes>
+          <Raffles />
+          <Footer />
       </div>
     </Router>
   );
