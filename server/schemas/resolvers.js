649dbe8be402b33a3e944c8e44f7878f6fd57a22
JacobRyanHanson <jacobryanhanson@gmail.com>
2022-02-03 20:42:00 -0600

    Update queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,37 +5,42 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-          if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id })
-              .select('-__v -password')
-              .populate('raffleTickets')
-              .populate('rafflesWon');
-    
-            return userData;
-          }
-    
-          throw new AuthenticationError('Not logged in');
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('raffleTickets')
+                    .populate('rafflesWon');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
         },
         users: async () => {
-          return User.find()
-            .select('-__v -password')
-            .populate('raffleTickets')
-            .populate('rafflesWon');
+            return User.find()
+                .select('-__v -password')
+                .populate('raffleTickets')
+                .populate('rafflesWon');
         },
         user: async (parent, { username }) => {
-          return User.findOne({ username })
-            .select('-__v -password')
-            .populate('raffleTickets')
-            .populate('rafflesWon');
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('raffleTickets')
+                .populate('rafflesWon');
         },
         raffleTickets: async (parent, { username }) => {
-          const params = username ? { username } : {};
-          return Ticket.find(params).sort({ createdAt: -1 });
+            return Ticket.find(username).sort({ createdAt: -1 });
         },
-        raffleTicket: async (parent, { _id }) => {
-          return Ticket.findOne({ _id });
+        raffleTicket: async (parent, { ticketId }) => {
+            return Ticket.findOne({ ticketId });
+        },
+        rafflesWon: async (parent, { username }) => {
+            return Ticket.find(username).sort({ createdAt: -1 });
+        },
+        raffles: async (parent, { ticketId }) => {
+            return Ticket.findOne({ ticketId });
         }
-      },
+    },
 }
 
 module.exports = resolvers;
