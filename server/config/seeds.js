37d379aea0b746b1480f6db56c3636423ecbf7f3
Alex C <alex.christianson.05@gmail.com>
2022-02-04 11:23:26 -0600

    added and testing seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,7 +1,27 @@
 const db = require('./connection');
+const faker = require('@faker-js/faker');
 const { User, Raffle, Ticket } = require('../models');
 
 db.once('open', async () => {
+    await User.deleteMany({});
+
+    // create user data
+    const userData = [];
+  
+    for (let i = 0; i < 10; i += 1) {
+        const firstName = faker.name.firstName();
+        const lastName = faker.name.lastName();
+        const username = faker.internet.userName();
+        const email = faker.internet.email(username);
+        const password = faker.internet.password();
+        const raffleTickets = [];
+        const rafflesWon = [];
+    
+        userData.push({ firstName, lastName, username, email, password, raffleTickets, rafflesWon });
+    }
+  
+    const createdUsers = await User.collection.insertMany(userData);
+
     await Raffle.deleteMany();
 
     const raffles = await Raffle.insertMany([
@@ -14,31 +34,60 @@ db.once('open', async () => {
         }
     ]);
 
-    console.log('Raffle items seeded');
+    await Ticket.deleteMany();
 
-    await User.deleteMany();
+    let createdTickets = [];
+    for (let i = 0; i < 100; i += 1) {
+      const ticketId = faker.number;
+  
+      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+  
+      const createdTicket = await Ticket.create({ ticketId, username });
+  
+      const updatedUser = await User.updateOne(
+        { _id: userId },
+        { $push: { raffleTickets: createdTicket._id } }
+      );
+  
+      createdTickets.push(createdTicket);
+    }
 
-    await User.create({
-        firstName: 'Pamela',
-        lastName: 'Halpert',
-        email: 'pbeasley@gmail.com',
-        username: 'vballgirl',
-        password: 'password123',
-        raffleTickets: [],
-        rafflesWon: []
-    });
+//     console.log('Raffle items seeded');
 
-    await User.create({
-        firstName: 'Dwight',
-        lastName: 'Schrute',
-        email: 'shrutefarm@gmail.com',
-        username: 'beetfarmer',
-        password: 'password123',
-        raffleTickets: [],
-        rafflesWon: []
-    });
+//     await User.deleteMany();
 
-    console.log('users seeded');
+//     await User.create({
+//         firstName: 'Pamela',
+//         lastName: 'Halpert',
+//         email: 'pbeasley@gmail.com',
+//         username: 'vballgirl',
+//         password: 'password123',
+//         raffleTickets: [
+//             {
+//                 tickets: [ticketArray[0]._id, ticketArray[1]._id]
+//             }
+//         ],
+//         rafflesWon: []
+//     });
 
+//     await User.create({
+//         firstName: 'Dwight',
+//         lastName: 'Schrute',
+//         email: 'shrutefarm@gmail.com',
+//         username: 'beetfarmer',
+//         password: 'password123',
+//         raffleTickets: [],
+//         rafflesWon: []
+//     });
+
+//     console.log('Users seeded');
+
+//    await Ticket.deleteMany();
+
+//     let ticketArray = [];
+//     for (let i = 0; i < 200; i += 1) {
+        
+//     }
     process.exit();
 });
