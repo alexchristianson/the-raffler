82f5fd345f430cb28478a6041175a7b05adf265f
Alex C <alex.christianson.05@gmail.com>
2022-02-03 20:26:14 -0600

    added id to ticket model

--- server/models/Ticket.js
+++ server/models/Ticket.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ticketSchema = new Schema({
+  id: {
+    type: Number,
+  },
   username: {
     type: Schema.Types.ObjectId,
     ref: 'User',
