6b2c8011f71a597673d662cf619009d9faf2244e
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2022-02-05 15:40:44 -0600

    added conditional rendering to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,52 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import LOGO from '../../assets/img/raffler-logo.png'
-
+import auth from '../../utils/auth'
 
 const Nav = () =>{
-    // function theNav(){
-    // if(Auth.loggedIn()){
-    //     return(
-    //         <nav className='nav-container'>
-            
-    //         <ul className='nav-items'>
-    //             <li>
-    //         <img className='logo-img' src={LOGO} alt='Raffler Text'/>
-    //         </li>
-    //             <li>
-    //                 Home
-    //             </li>
-    //             <li>
-    //                 Something
-    //             </li>
-    //             <li className='login-btn'>
-    //                 Login
-    //             </li>
-    //         </ul>
-    //         </nav>
-    //             )
-    //         } else{
-    //             return(
-    //                 <nav className='nav-container'>
-                    
-    //                 <ul className='nav-items'>
-    //                     <li>
-    //                 <img className='logo-img' src={LOGO} alt='Raffler Text'/>
-    //                 </li>
-    //                     <li>
-    //                         Home
-    //                     </li>
-    //                     <li>
-    //                         Something
-    //                     </li>
-    //                     <li className='login-btn'>
-    //                         logout
-    //                     </li>
-    //                 </ul>
-    //                 </nav>
-    //                     )
-    //                 }
-    //             }
+
+    const logout = event => {
+        event.preventDefault();
+        auth.logout();
+      };
+    if(auth.loggedIn()){
+   
     return(
 <nav className='nav-container'>
 
@@ -63,13 +27,39 @@ const Nav = () =>{
     <li>
         [PLACEHOLDER]
     </li>
-    <a href='/login' className='login-btn' style={{textDecoration: 'none'}}>Login</a>
+    <a href='/' className='login-btn' style={{textDecoration: 'none'}}  onClick={logout}>Logout</a>
     {/* <li className='login-btn'>
         <a href='/login'>Login</a>
     </li> */}
 </ul>
 </nav>
     )
+}
+else{
+    return(
+        <nav className='nav-container'>
+        
+        <ul className='nav-items'>
+            <li>
+        <Link to="/">
+        <img className='logo-img' src={LOGO} alt='Raffler Text'/>
+        </Link>
+        </li>
+           <a href='/'> <li>
+                Home
+            </li>
+            </a>
+            <li>
+                [PLACEHOLDER]
+            </li>
+            <a href='/login' className='login-btn' style={{textDecoration: 'none'}}>Login</a>
+            {/* <li className='login-btn'>
+                <a href='/login'>Login</a>
+            </li> */}
+        </ul>
+        </nav>
+            )
+}
 }
 
 
