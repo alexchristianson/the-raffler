b9fb7f942474a9a83460c73c6421e7ee0bdb9602
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2022-02-05 13:37:21 -0600

    visual changed to home and nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,52 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Footer from './components/Footer';
+import Raffles from './components/Raffles';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <div>
-          <Nav />
-          <Routes>
-            <Route exact path='/login' element={<Login/>}/>
-            <Route exact path='/signup' element={<SignUp/>}/>
-          </Routes>
-      </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+            <Nav />
+            <Routes>
+              <Route exact path='/login' element={<Login/>}/>
+              <Route exact path='/signup' element={<SignUp/>}/>
+            </Routes>
+            <Raffles />
+        </div>
     </Router>
+    </ApolloProvider>
   );
 }
 
