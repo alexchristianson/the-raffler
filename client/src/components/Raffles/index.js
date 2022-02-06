ea7520b2068f5731e4eef5a5b198ba98c047df87
ethan fregien <ethanfregien@gmail.com>
2022-02-05 19:23:56 -0600

    first grid row of raffles

--- client/src/components/Raffles/index.js
+++ client/src/components/Raffles/index.js
@@ -16,7 +16,11 @@ import Typography from '@mui/material/Typography';
 
 function CardContainer() {
     // react makes a call for DATA (from cache, DB)  --> look into HOOKS Apollo HOOK
-    let DATA = [{title: "Cucumber", description: "Sweet Savory Cucumber", image: img1 }, {name: "Shrek Beard", description: "Shrek Beard with matching fedora", image: img2 }, {name: "Payday loan", description: "Payday loan ranging from $5-500", image: img3}, {name: "Angry Cat", description: "Angry fluffy cat", image: img4}, {name: "Quarter", description: "Shiny new quarter", image: img5}]; // --> Think of this as STATE
+    let DATA = [{title: "Cucumber", description: "Sweet Savory Cucumber", image: img1 }, 
+    {title: "Shrek Beard", description: "Shrek Beard with matching fedora", image: img2 }, 
+    {title: "Payday loan", description: "Payday loan ranging from $5-500", image: img3}, 
+    {title: "Angry Cat", description: "Angry fluffy cat", image: img4}, 
+    {title: "Quarter", description: "Shiny new quarter", image: img5}]; // --> Think of this as STATE
     let firstItem = DATA[0];
     let secondItem = DATA[1];
     let thirdItem = DATA[2];
@@ -25,16 +29,16 @@ function CardContainer() {
     return (
         // How do we we return multiple cards?  --> think FOR loop
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
+        <Grid item xs={3}>
         <CardStock data = {firstItem}/>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={3}>
         <CardStock data = {secondItem}/>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={3}>
         <CardStock data = {thirdItem}/>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={3}>
         <CardStock data = {fourthItem}/>
         </Grid>
         </Grid>
