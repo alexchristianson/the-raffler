191d51ae98c7dd402fbeedf281fd1b3b51b91717
ethan fregien <ethanfregien@gmail.com>
2022-02-08 19:19:23 -0600

    starting function to return users raffles when logged in

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 function User() {
     
+    return (<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {  && ( (item) => (
+            <Grid item xs={3}>
+                <CardStock data = {}/>
+            </Grid>
+        ))}
+    </Grid>)
 }
 
 export default User;
