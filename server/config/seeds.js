25a4d48d9eb0d78d6a83dad0f21839265129382d
Alex C <alex.christianson.05@gmail.com>
2022-02-08 21:39:43 -0600

    updated image routes

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -43,42 +43,42 @@ db.once('open', async () => {
         {
             name: 'Dodge Caravan',
             description: '2008 Dodge Caravan in fair condition',
-            image: "../../client/src/assets/img/caravan.jpg",
+            image: "../src/assets/img/caravan.jpg",
             ticketArray: [],
             timer: 400000
         },
         {
             name: 'Golf Balls',
             description: 'A bucket of 100 golf balls.',
-            image: "../../client/src/assets/img/balls.jpg",
+            image: "../src/assets/img/balls.jpg",
             ticketArray: [],
             timer: 500000
         },
         {
             name: 'Autographed Sword',
             description: 'Katana signed by Randy Jackson',
-            image: "../../client/src/assets/img/katana.jpg",
+            image: "../src/assets/img/katana.jpg",
             ticketArray: [],
             timer: 600000
         },
         {
             name: 'Velvet Painting',
             description: 'A velvet painting of Spock from Star Trek.',
-            image: "../../client/src/assets/img/spock.jpg",
+            image: "../src/assets/img/spock.jpg",
             ticketArray: [],
             timer: 700000
         },
         {
             name: 'Gift Certificate',
             description: '$50 gift certificate to Taco Bell.',
-            image: "../../client/src/assets/img/taco-bell.png",
+            image: "../src/assets/img/taco-bell.png",
             ticketArray: [],
             timer: 800000
         },
         {
             name: 'Vacation Getaway',
             description: 'All expense paid trip for 2 to Oshkosh, WI.',
-            image: "../../client/src/assets/img/oshkosh.jpg",
+            image: "../src/assets/img/oshkosh.jpg",
             ticketArray: [],
             timer: 900000
         },
