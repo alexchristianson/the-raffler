8bdafd810980b008ca512ab360c356dde6849874
ethan fregien <ethanfregien@gmail.com>
2022-02-08 20:41:17 -0600

    changing ruery to dat.map

--- client/src/components/Raffles/index.js
+++ client/src/components/Raffles/index.js
@@ -4,10 +4,7 @@ import CardStock from '../CardStock';
 import {Grid} from '@mui/material';
 
 function CardContainer() {
-    // react makes a call for DATA (from cache, DB)  --> look into HOOKS Apollo HOOK
     console.log(useQuery, QUERY_ALL_RAFFLES);
-
-    // This will query the API (backend) Database for Data
     const { loading, data } = useQuery(QUERY_ALL_RAFFLES);  // remember this is an async process
     console.log(data);  // this will be 'undefined' UNTIL loading completes and puts the info into 'data'
     const raffleData = data?.me || [];  // Until we get data loaded into 'raffleData' we initialize an empty array []
@@ -16,7 +13,7 @@ function CardContainer() {
 
     return (
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            { raffleData && raffleData.map( (item) => (
+            { data && data.map( (item) => (
                 <Grid item xs={3}>
                     <CardStock data = {item}/>
                 </Grid>
