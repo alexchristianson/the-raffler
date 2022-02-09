4f32841c9a41846c565ed3e3e63d043832aa6862
Alex C <alex.christianson.05@gmail.com>
2022-02-09 14:55:32 -0600

    trying to fix login and signup links so heroku works

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,30 +7,26 @@ import CountDownTimer from "../components/CountDownTimer";
 const Home = () => {
   const hoursMinSecs = {hours:1, minutes: 20, seconds: 40}
   if(auth.loggedIn()){
-
-    
     return(
       <div>
           <Raffles/>
       </div>
     )
-  }else{
-  return (
-    <div className="dash-container">
-
-<div>
-  <h1 className="dash-text">Feeling Lucky? <br/>
-    Start Winning!
-  </h1>
-<a href="/signup"><button className="tick-btn">Get Tickets</button></a> 
-</div>
-<img  className="raffle-img"src={raffleimg} alt="Raffle Machine"/>
-<div>
-
-</div>
-<CountDownTimer hoursMinSecs={hoursMinSecs}/>
+  } else {
+    return (
+      <div className="dash-container">
+        <div>
+          <h1 className="dash-text">Feeling Lucky? <br/>
+            Start Winning!
+          </h1>
+        <a href="/signup"><button className="tick-btn">Get Tickets</button></a> 
+        </div>
+        <img  className="raffle-img"src={raffleimg} alt="Raffle Machine"/>
+        <div>   
+      </div>
+    <CountDownTimer hoursMinSecs={hoursMinSecs}/>
     </div>
-  );
+    );
   }
 };
 
